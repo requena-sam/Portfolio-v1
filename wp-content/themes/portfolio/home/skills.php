0eb94cc1cf40ee5014385ee788684b684afaaded
requena-sam <145543184+requena-sam@users.noreply.github.com>
2024-05-16 18:57:59 +0200

    Adding skills component to home file

---
+++ wp-content/themes/portfolio/home/skills.php
@@ -0,0 +1,14 @@
+<section class="skills">
+    <h2><?= get_field('skill-title') ?></h2>
+    <div class="skills__container">
+        <?php if (have_rows('skill-list')) :
+            while (have_rows('skill-list')) : the_row();
+                $text = get_sub_field('text');
+                $image = get_sub_field('image');
+                ?>
+                <img src="<?= $image; ?>" alt="logo de <?= $text; ?>">
+            <?php endwhile;
+        endif; ?>
+    </div>
+
+</section>
