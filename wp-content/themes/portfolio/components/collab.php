cd1c10a337ba900a235bd15600f0a4158f44bf26
requena-sam <145543184+requena-sam@users.noreply.github.com>
2024-05-17 22:41:04 +0200

    Update structure

--- wp-content/themes/portfolio/components/collab.php
+++ wp-content/themes/portfolio/components/collab.php
@@ -1,8 +1,8 @@
 <div class="collab">
-    <h3>
+    <h3 class="collab__title">
         <?= get_field('collab-title', 'option'); ?>
     </h3>
-    <p><?= get_field('collab-text', 'option'); ?></p>
+    <p class="collab__text"><?= get_field('collab-text', 'option'); ?></p>
     <div class="collab__cta cta_group">
         <a class="cta_group__primary_btn" href="<?= get_field('collab-link1', 'option'); ?>">Me contacter</a>
         <a class="cta_group__secondary_btn" href="<?= get_field('collab-link2', 'option'); ?>">Mes projets</a>
