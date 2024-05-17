1b1b4db844f143cda28a0f06d201b4a645896ca3
requena-sam <145543184+requena-sam@users.noreply.github.com>
2024-05-17 13:00:58 +0200

    Update my about html structure

--- wp-content/themes/portfolio/template-about.php
+++ wp-content/themes/portfolio/template-about.php
@@ -2,36 +2,42 @@
 <?php get_header(); ?>
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
     <main>
-        <h2 role="heading" aria-level="2"><?= get_field('about-title'); ?></h2>
+        <h2 role="heading" aria-level="2" class="main_title"><?= get_field('about-title'); ?></h2>
         <section class="about">
-            <img src="<?= get_field('about-image'); ?>" alt="Image de moi" width="30%">
-            <h3 role="heading" aria-level="3"><?= get_field('name'); ?>
-                <span class="about__desc"><?= get_field('description'); ?></span>
-            </h3>
-            <p class="about__content"><?= get_field('content'); ?></p>
+            <div class="about__img">
+                <img src="<?= get_field('about-image'); ?>" alt="Image de moi" width="30%">
+            </div>
+            <div class="about__content">
+                <h3 role="heading" aria-level="3"><?= get_field('name'); ?>
+                    <span class="about__desc"><?= get_field('description'); ?></span>
+                </h3>
+                <p class="about__content"><?= get_field('content'); ?></p>
+            </div>
         </section>
         <section class="parcours">
             <h2 role="heading" aria-level="2" class="parcours__title">
                 <?= get_field('parcours-title'); ?>
             </h2>
-            <?php if (have_rows('parcours-list')) :
-            while (have_rows('parcours-list')) :
-            the_row();
-            $schoolName = get_sub_field('school');
-            $duration = get_sub_field('duration');
-            $desc = get_sub_field('description');
-            ?>
-            <div class="parcours__item">
-                <h3 class="parcours__item__school" role="heading" aria-level="3">
-                    <?= $schoolName; ?>
-                </h3>
-                <span class="parcours__item__duration">
+            <div class="parcours__container">
+                <?php if (have_rows('parcours-list')) :
+                    while (have_rows('parcours-list')) :
+                        the_row();
+                        $schoolName = get_sub_field('school');
+                        $duration = get_sub_field('duration');
+                        $desc = get_sub_field('description');
+                        ?>
+                        <div class="parcours__container__item">
+                            <h3 class="parcours__container__item__school" role="heading" aria-level="3">
+                                <?= $schoolName; ?>
+                            </h3>
+                            <span class="parcours__container__item__duration">
                             <?= $duration; ?>
                         </span>
-                <p class="parcours__item__desc">
-                    <?= $desc; ?>
-                </p>
-                <?php endwhile;
+                            <p class="parcours__container__item__desc">
+                                <?= $desc; ?>
+                            </p>
+                        </div>
+                    <?php endwhile;
                 endif; ?>
             </div>
         </section>
@@ -44,7 +50,6 @@
                         $stepTitle = get_sub_field('title');
                         $stepDescription = get_sub_field('description');
                         ?>
-
                         <li>
                             <article>
                                 <h3><?= $stepTitle; ?></h3>
