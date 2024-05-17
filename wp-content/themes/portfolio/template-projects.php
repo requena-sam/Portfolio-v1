997b560ba1e823a5fec0dda7b0af0fb058843063
requena-sam <145543184+requena-sam@users.noreply.github.com>
2024-05-17 13:00:44 +0200

    Update my projects html structure

--- wp-content/themes/portfolio/template-projects.php
+++ wp-content/themes/portfolio/template-projects.php
@@ -2,7 +2,9 @@
 <?php get_header(); ?>
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
     <main>
-            <h2 role="heading" aria-level="2">Mes projets</h2>
+        <h2 role="heading" aria-level="2" class="main_title">Mes projets</h2>
+        <section class="projects">
+            <h3 class="hidden" role="heading" aria-level="3">Liste de mes projets</h3>
             <?php if (have_rows('project-list')) :
                 while (have_rows('project-list')) : the_row();
                     $title = get_sub_field('title');
@@ -11,20 +13,25 @@
                     $image = get_sub_field('image');
                     $link = get_sub_field('link');
                     ?>
-                    <a href="<?= $link ;?>">
-                        <article class="project" itemscope itemtype="http://schema.org/CreativeWork" tabindex="0">
-                            <div class="project__content">
+                    <a class="hover_card" href="<?= $link; ?>">
+                        <article class="projects__card" itemscope itemtype="http://schema.org/CreativeWork"
+                                 tabindex="0">
+                            <div class="projects__card__content">
                                 <h3 role="heading" aria-level="3" itemprop="name"><?= $title; ?></h3>
+                                <span itemprop="genre" class="projects__card__content__category_web"><?= $category; ?></span>
                                 <p itemprop="description"><?= $text; ?></p>
-                                <span itemprop="genre"><?= $category; ?></span>
                             </div>
-                            <img src="<?= $image; ?>" itemprop="image"
-                                 alt="Image representative du projet" width="300px">
+                            <div class="projects__card__img">
+                                <img src="<?= $image; ?>" itemprop="image"
+                                     alt="Image representative du projet" width="300px">
+                            </div>
                         </article>
                     </a>
                 <?php endwhile;
             endif;
             ?>
+            <p class="projects__end">Vous êtes arrivé au bout</p>
+        </section>
     </main>
 <?php endwhile; endif; ?>
 <?= get_footer(); ?>
