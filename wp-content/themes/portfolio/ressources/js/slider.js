727f53ec934a9f85be0ac138434da007632d7011
requena-sam <145543184+requena-sam@users.noreply.github.com>
2024-06-18 18:16:21 +0200

    Update px to rem and slider fix bullet

--- wp-content/themes/portfolio/ressources/js/slider.js
+++ wp-content/themes/portfolio/ressources/js/slider.js
@@ -2,8 +2,10 @@ export const slider = {
     init() {
         this.container = document.querySelector('.reviews__slider__container');
         this.bullets = document.querySelectorAll('.bullet');
+        this.reviews = document.querySelectorAll('.reviews__slider__container__article');
         this.bullets[0].classList.add('active');
         this.addEventListeners();
+        this.addScrollListener();
     },
     addEventListeners() {
         this.bullets.forEach(bullet => {
@@ -12,6 +14,17 @@ export const slider = {
             });
         });
     },
+    addScrollListener() {
+    this.container.addEventListener('scroll', () => {
+        const scrollPosition = this.container.scrollLeft;
+        const firstReviewWidth = this.reviews[0].offsetWidth;
+        if (scrollPosition > firstReviewWidth && this.bullets[1]) {
+            this.updateActiveBullet(1);
+        } else if (scrollPosition < firstReviewWidth) {
+            this.updateActiveBullet(0);
+        }
+    });
+},
     handleBulletClick(bullet) {
         const index = bullet.getAttribute('data-index');
         const newScrollPosition = this.container.scrollWidth * index / this.bullets.length;
